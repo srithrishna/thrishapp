62c75d9d55b9a8bc81ed921dd1b7e5ba3e2ad3a4
srithrishna <thrishna23jayakumar@gmail.com>
2023-08-08 11:27:10 +0530

    size

--- src/components/Biodata.js
+++ src/components/Biodata.js
@@ -15,7 +15,7 @@ const Biodata = () => {
         <p><b>Address:</b> V G Rao Nagar, Udumalpet, Tiruppur
         <br /> 642 126</p>
       </div>
-      <div style={{ flex: '0 0 48%', marginBottom: '20px' }}>
+      <div style={{ flex: '0 0 48%', marginBottom: '20px', marginLeft:'30px' }}>
         <h2>Education</h2>
         <p><b>12th - Biology</b></p>
         <p>Srinivasa Vidhyalaya Matric Higher Secondary School</p>
@@ -31,7 +31,7 @@ const Biodata = () => {
         <p><b>projects: </b>Measuring Heart Beat Using Heart Beat Sensor</p>
         <p>our project journey exemplified a top-to-bottom approach, where we started with planning, <br />design, and architecture at the top and worked our way down through development, testing,<br /> and deployment. This comprehensive strategy allowed us to create a well-structured, <br />feature-rich, and reliable project that met all its intended objectives." </p>
       </div>
-      <div style={{ flex: '0 0 48%', marginBottom: '20px', marginLeft:'20px' }}>
+      <div style={{ flex: '0 0 48%', marginBottom: '20px', marginLeft:'30px' }}>
         <h2>Achievements @ School</h2>
         <p><b>Proficiency Rank Holder</b></p>
         <p>I Have been awarded Proficiency Rank holer for more than five time for being at top 3 ranks. </p>
@@ -49,7 +49,7 @@ const Biodata = () => {
           <li>Node.js</li>
         </ul>
       </div>
-      <div style={{ flex: '0 0 48%', marginBottom: '20px' }}>
+      <div style={{ flex: '0 0 48%', marginBottom: '20px', marginLeft:'30px' }}>
         <h2>Hobbies</h2>
         <ul>
           <li>Reading Books</li>
